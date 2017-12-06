54e4b92cb347e9b4b7780d62961ca8f49912eeb9
Joshua Seitz <SeitzJoshua7@gmail.com>
2017-12-05 22:20:48 -0500

    added private method and regular expression

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,82 @@
+class EmailsController < ApplicationController
+  before_action :set_email, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @emails = Email.all
+  end
+
+  def show
+    @delivered_to = get_delivered_to(@email)
+    @message_id = get_message_id(@email)
+    @email_subject = get_email_subject(@email)
+    @email_from = get_email_from(@email)
+    @email_received = get_email_received(@email)
+  end
+
+  def new
+    @email = Email.new
+  end
+
+  def edit
+  end
+
+  def create
+    @email = Email.new(email_params)
+
+    respond_to do |format|
+      if @email.save
+        format.html { redirect_to @email, notice: 'Email was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @email.update(email_params)
+        format.html { redirect_to @email, notice: 'Email was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to emails_url, notice: 'Email was successfully destroyed.' }
+    end
+  end
+
+  private
+
+    def set_email
+      @email = Email.find(params[:id])
+    end
+
+    def email_params
+      params.require(:email).permit(:text)
+    end
+
+    def get_delivered_to(email)
+      email.text.match(/Delivered-To: .*/)
+    end
+
+    def get_message_id(email)
+      email.text.match(/Message-ID: .*/)
+    end
+
+    def get_email_subject(email)
+      email.text.match(/Subject: .*/)
+    end
+
+    def get_email_from(email) 
+      email.text.match(/From: .*/)
+    end
+
+    def get_email_received(email)
+      email.text.match(/(Received: by) .+\n(.+)/)
+    end
+
+end
